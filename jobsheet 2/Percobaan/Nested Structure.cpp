df66eeb42a7f46a859f74538416b060f267a9925
joelws2 <144107442+joelws2@users.noreply.github.com>
2024-06-19 18:34:21 +0700

    Add files via upload

---
+++ jobsheet 2/Percobaan/Nested Structure.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+struct complex
+{
+	int imag;
+	float real;
+};
+struct number 
+{
+	struct complex comp;
+	int integers;
+}num1, num2;
+//buatlah nested struct sampai dengan 5 tingkat
+int main()
+{
+}
